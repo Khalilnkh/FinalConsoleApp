91fc2d5d2b004ef5fbc72fe5884516a75283f232
Khalilnkh <khalilnkh@code.edu.az>
2023-09-15 11:10:51 +0900

    CreatedFinalConsoleMarketProject

---
+++ MarketConsoleApp/MarketConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace MarketConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int selectedOption;
+
+            Console.WriteLine("Welcome to Market Console App!");
+
+            do
+            {
+                Console.WriteLine("1. Operations on Products");
+                Console.WriteLine("2. Operations on Sales");
+                Console.WriteLine("3. Exit");
+
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Please, select an option:");
+
+                while (!int.TryParse(Console.ReadLine(), out selectedOption))
+                {
+                    Console.WriteLine("Please enter valid option:");
+                }
+
+                switch (selectedOption)
+                {
+                    case 1:                      
+                        break;
+                    case 2:                       
+                        break;
+                    case 3:
+                        Console.WriteLine("Bye!");
+                        break;                  
+                    default:
+                        Console.WriteLine("No such option!");
+                        break;
+                }
+            } while (selectedOption != 0);
+        }
+    }
+}
