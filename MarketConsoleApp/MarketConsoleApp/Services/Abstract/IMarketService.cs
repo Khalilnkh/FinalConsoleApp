f5a6bbe3be0ab1f0e26e2dd6c7f39a084dd63182
Khalilnkh <khalilnkh@code.edu.az>
2023-09-15 13:56:01 +0900

    ProductMethodsCreatedForMarketConsoleApp

---
+++ MarketConsoleApp/MarketConsoleApp/Services/Abstract/IMarketService.cs
@@ -0,0 +1,25 @@
+﻿using MarketConsoleApp.Data.Enums;
+using MarketConsoleApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketConsoleApp.Services.Abstract
+{
+    public interface IMarketService
+    {
+        public List<Product> GetProducts();
+        public int AddProduct(string name,decimal price,Department department,int quantity);
+        public int UpdateProduct(int id, string name, decimal price, Department department, int quantity);
+        public List<Product> GetProductsByCategory(int id, string name, decimal price, Department department, int quantity);
+        public List<Product> GetProductsByPricerange(int id, string name, decimal price, Department department, int quantity);
+        public List<Product> GetProductsByGivenName(int id, string name, decimal price, Department department, int quantity);
+        public int DeleteProduct(int id);
+
+
+        
+
+    }
+}
